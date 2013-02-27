e00e35be4375987367bfb254b04207b5b8562e61
Eric Johnson <eric.git@iijo.org>
2013-02-27 14:10:27 +0000

    rm selected and deselected attrs

--- lib/FlashCards/Model/SetDefinition.pm
+++ lib/FlashCards/Model/SetDefinition.pm
@@ -7,24 +7,11 @@ use FlashCards::Model::SetDefinition;
 use MooseX::Params::Validate;
 with 'FlashCards::Model';
 
-
 my $schema = FlashCards::Model::Schema->Schema;
 has_table $schema->table('SetDefinition');
-has_one   'set'        => (table => $schema->table('SetOfCards'));
-has_one   'definition' => (table => $schema->table('Definition'));
-has_one   'author'     => (table => $schema->table('User'));
-
-has 'selected' => (
-   is      => 'rw',
-   isa     => 'Int',
-   default => 1,
-);
-
-has 'disabled' => (
-   is      => 'rw',
-   isa     => 'Int',
-   default => 1,
-);
+has_one set        => (table => $schema->table('SetOfCards'));
+has_one definition => (table => $schema->table('Definition'));
+has_one author     => (table => $schema->table('User'));
 
 sub add {
    my $self   = shift or die;
