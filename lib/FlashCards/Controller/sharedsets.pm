c5aa9c0d9d3eb28d153e09632032aeafef4d0eae
Eric Johnson <eric.git@iijo.org>
2013-03-02 20:15:37 +0000

    rewrite lots

--- lib/FlashCards/Controller/sharedsets.pm
+++ lib/FlashCards/Controller/sharedsets.pm
@@ -1,46 +1,53 @@
 package FlashCards::Controller::sharedsets;
 
+use 5.12.0;
 use strict;
 use warnings;
 use base 'Catalyst::Controller';
 
 use FlashCards::Model::SetOfCards;
+use FlashCards::Model::PopularSets;
 use Fey::DBIManager;
 use List::MoreUtils qw/any/;
 
 sub sharedsets : Path('/sharedsets') Args {
     my ($self, $c, $page) = @_; 
-    $c->forward('FlashCards::Controller::sharedsets', 'common', [$page]);
-}
-
-sub common {
-    my ($self, $c, $page, $letter) = @_;
-    $page = 0 
-       if !defined $page;
 
     $c->user->userId;
 
-    my %args = (
-       page    => $page,
-       orderBy => 'name',
-    );
-    $args{name}   = $letter . "%" if any { $_ eq $letter } "a".."z";
-    $args{number} = 1             if $letter eq "numbers";
+    my @sets = FlashCards::Model::PopularSets
+        ->selectAllAsUserSets(page => $page || 0)->all;
 
-    my @sets = FlashCards::Model::SetOfCards->selectAll(%args)->all;
-
-    $c->stash->{morePages} = 1
-       if $c->config->{pageSize} == scalar(@sets);
-    $c->stash->{page}     = $page;
-    $c->stash->{sets}     = \@sets;
-    $c->stash->{template} = 'sharedsets/view.tt';
-    $c->stash->{alphabet} = ["a".."z"];
-    $c->stash->{currentLetter} = $letter;
+    $self->finish($c, \@sets, $page);
 }
 
 sub byletter : Path('/sharedsets/byletter') {
     my ($self, $c, $letter, $page) = @_;
-    $c->forward('FlashCards::Controller::sharedsets', 'common', [$page, $letter]);
+
+    $c->user->userId;
+
+    my %args = (
+       page    => $page || 0,
+       orderBy => 'name',
+    );
+    $args{name}   = $letter . "%" if $letter && any { $_ eq $letter } "a".."z";
+    $args{number} = 1             if $letter && $letter eq "numbers";
+
+    my @sets = FlashCards::Model::SetOfCards->selectAll(%args)->all;
+
+    $self->finish($c, \@sets, $page);
+
+    $c->stash->{currentLetter} = $letter;
+}
+
+sub finish {
+    my ($self, $c, $sets, $page) = @_;
+
+    $c->stash->{morePages} = 1 if $c->config->{pageSize} == scalar(@$sets);
+    $c->stash->{page}      = $page || 0;
+    $c->stash->{sets}      = $sets;
+    $c->stash->{alphabet}  = ["a".."z"];
+    $c->stash->{template}  = 'sharedsets/view.tt';
 }
 
 1;
