aecac03acea509099ef087f728b6781469b8c85d
Eric Johnson <eric.git@iijo.org>
2013-11-29 06:20:17 +0000

    fixed a comment

--- lib/FlashCards/Controller/set/definition.pm
+++ lib/FlashCards/Controller/set/definition.pm
@@ -56,7 +56,7 @@ sub add : Chained('definition') Args(0) {
    $c->res->redirect("/set/$setId/" . uri_escape($userSet->set->name));
 }
 
-# only the set owner has permission to add definitions
+# only the set owner has permission to remove definitions
 sub remove : Chained('definition') Args(0) {
    my ($self, $c) = @_; 
    my $setId        = $c->stash->{setId};
@@ -72,7 +72,7 @@ sub remove : Chained('definition') Args(0) {
       setId        => $setId,
       definitionId => $definitionId,
    );
-   Catalyst::Exception->throw("you don't have permission to delete this word because you aren't the person who added the word to this set")
+   Catalyst::Exception->throw("you don't have permission to delete this word because you aren't the set owner")
       unless $c->user->userId == $setDefinition->authorId;
 
    $setDefinition->remove(
